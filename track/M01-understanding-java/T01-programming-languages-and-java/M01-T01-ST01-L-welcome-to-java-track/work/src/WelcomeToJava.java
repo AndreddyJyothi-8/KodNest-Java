46f0cb7a5c123368853b437d9900a221f4195028
Jyothi <andreddyjyothi@gmail.com>
2026-07-30 12:50:02 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        System.out.println("Welcome to the world of Java!");
+        System.out.println("I am excited to start this journey of learning Java.");
+        System.out.println("Now I am  learning how to express that logic using java.");
     }
 }
