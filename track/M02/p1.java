da2953de84eef9535388c973394c7e45ff173551
Jyothi <andreddyjyothi@gmail.com>
2026-08-04 12:25:33 +0530

    one more

---
+++ track/M02/p1.java
@@ -0,0 +1,14 @@
+
+public class p1 {
+
+    public static void main(String[] args) {
+        int num = -1;
+        if (num > 0) {
+            System.out.println("Positive number");
+        } else if (num < 0) {
+            System.out.println("Negative number");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
