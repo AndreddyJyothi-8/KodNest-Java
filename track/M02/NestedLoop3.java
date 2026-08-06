67caa670cfc060dc2e38092381f984530626a33c
Jyothi <andreddyjyothi@gmail.com>
2026-08-06 15:07:52 +0530

    doWhile

---
+++ track/M02/NestedLoop3.java
@@ -0,0 +1,11 @@
+
+public class NestedLoop3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        do {
+            System.out.println(i);
+        } while (i <= 5);
+        i++;
+    }
+}
