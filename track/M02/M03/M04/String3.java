0cbb944d2c73a86634659515bde8e2c65ca85e52
Jyothi <andreddyjyothi@gmail.com>
2026-09-07 17:24:56 +0530

    String3

---
+++ track/M02/M03/M04/String3.java
@@ -0,0 +1,15 @@
+
+public class String3 {
+
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "Java";
+        if (s1.equals(s2)) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
